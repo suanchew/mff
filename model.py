ef378beb7437c965d14d9f001c7bd0a7bfcc3b97
evlyn57 <evlyn57@gmail.com>
2016-09-10 12:49:36 -0700

    tables created

--- model.py
+++ model.py
@@ -12,32 +12,38 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    """User of petition website."""
+    """User of website."""
 
     # create table named users with appropriate columns
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(60), nullable=False)
     last_name = db.Column(db.String(60), nullable=False)
-    zipcode = db.Column(db.Integer, nullable=False)
+    phone_number = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(60), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
+    profile_photo = db.Column(db.String(500), nullable=True)
 
 
-class SavedPetition(db.Model):
-    """Petitions that users have saved or signed"""
+class Places(db.Model):
+    """Places"""
 
-    # create table named saved_petitions with appropriate columns
-    __tablename__ = 'saved_petitions'
-    saved_petition_id = db.Column(db.Integer, primary_key=True,
+    # create table named places with appropriate columns
+    __tablename__ = 'places'
+    places_id = db.Column(db.Integer, primary_key=True,
                                   autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    petition_id = db.Column(db.Integer, nullable=False)
-    date_signed = db.Column(db.DateTime, nullable=True)
-    # define relationship to user
-    users = db.relationship('User',
-                            backref=db.backref('saved_petitions',
-                                               order_by=saved_petition_id))
+    name = db.Column(db.String(60), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1000), nullable=True)
+    open_time = db.Column(db.String(20), nullable=True)
+    close_time = db.Column(db.String(20), nullable=True)
+    place_photo = db.Column(db.String(500), nullable=True)
+    seating = db.Column(db.Boolean, nullable=True)
+    restroom = db.Column(db.Boolean, nullable=True)
+    wifi = db.Column(db.Boolean, nullable=True)
+    wheelchair_accessible = db.Column(db.Boolean, nullable=True)
+    url = db.Column(db.String(100), nullable=True)
+
 
 
 ##############################################################################
